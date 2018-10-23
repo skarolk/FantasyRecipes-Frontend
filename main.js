5054ea2080a16eea7387ff95fc78be32c0abd21a
skarolk <skarolkiewicz@gmail.com>
2018-10-23 15:08:00 -0400

    fixed MVP

--- main.js
+++ main.js
@@ -2,19 +2,19 @@
 
   var bv = new Bideo();
   bv.init({
-
     videoEl: document.querySelector('#background_video'),
-
     container: document.querySelector('body'),
-
     resize: true,
 
     isMobile: window.matchMedia('(max-width: 768px)').matches,
 
+    playButton: document.querySelector('#play'),
+    pauseButton: document.querySelector('#pause'),
+
     src: [
       {
         src: 'night.mp4',
-      },
+      }
     ],
 
     onLoad: function () {
